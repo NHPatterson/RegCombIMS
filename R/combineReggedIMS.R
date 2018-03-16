fd402fab5a5830ea489b1efc0651e92852686f7a
Patterson <pattenh1@vanderbilt.edu>
2018-03-16 11:19:02 -0500

    fixed coordinate ordering issue at combine

--- R/combineReggedIMS.R
+++ R/combineReggedIMS.R
@@ -25,26 +25,29 @@ combineReggedIMS <- function(cardinaldata1,cardinaldata2,ds1_name = "dataset1",d
   cardinaldata1 <- cardinaldata1[,ds1_retained_pixels]
   cardinaldata2 <- cardinaldata2[,ds2_retained_pixels]
   
+  ##get data in y,x order so pixel rows are matched at data combination 
+  cd1_reordering = order(cardinaldata1$y,cardinaldata1$x)
+  cd2_reordering = order(cardinaldata2$y,cardinaldata2$x)
+  
+  
   if ('ds_origin' %in% varLabels(cardinaldata1@featureData)){
   } else {
     cardinaldata1@featureData$ds_origin <- ds1_name
   }
   
   cardinaldata2@featureData$ds_origin <- ds2_name
-
+  
   mz_sortvector <- sort(c(Cardinal::mz(cardinaldata1),Cardinal::mz(cardinaldata2)),index.return=T)
-
+  
   ##make new MSIageSet:
-  combined <- MSImageSet(spectra=rbind(iData(cardinaldata1), 
-                                         iData(cardinaldata2))[mz_sortvector$ix,], 
-                          coord=data.frame(coord(cardinaldata1)[c("x","y")],sample=combined_name,row.names=NULL), 
-                          mz=mz_sortvector$x,
-                          processingData = processingData(cardinaldata1),
-                          protocolData = protocolData(cardinaldata1),
-                          experimentData = experimentData(cardinaldata1))
-  
-  
-  combined@featureData$ds_origin = c(cardinaldata1@featureData$ds_origin, cardinaldata2@featureData$ds_origin)[mz_sortvector$ix]
+  combined <- MSImageSet(spectra=rbind(iData(cardinaldata1)[,cd1_reordering], 
+                                       iData(cardinaldata2)[,cd2_reordering])[mz_sortvector$ix,], 
+                         coord=data.frame(coord(cardinaldata1)[cd1_reordering,][c("x","y")],
+                                          sample=combined_name,row.names=NULL), 
+                         mz=mz_sortvector$x,
+                         processingData = processingData(cardinaldata1),
+                         protocolData = protocolData(cardinaldata1),
+                         experimentData = experimentData(cardinaldata1))
   
   return(combined)
 }
